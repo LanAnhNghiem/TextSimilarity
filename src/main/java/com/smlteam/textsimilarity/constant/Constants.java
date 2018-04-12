b7c975a24f00e40f6d6e7e2e90b7d36f1fea1cde
LanAnhNghiem <lananh.nghiem0502@gmail.com>
2018-04-12 22:55:19 +0700

    Index documents

--- src/main/java/com/smlteam/textsimilarity/constant/Constants.java
+++ src/main/java/com/smlteam/textsimilarity/constant/Constants.java
@@ -1,6 +1,8 @@
 package com.smlteam.textsimilarity.constant;
 
 public class Constants {
-    public static final String TEST = "/home/lana/IdeaProjects/TextSimilarity/src/main/resources/test/data.txt";
-
+    public static final String ORIGIN = "src/main/resources/test/data1.txt";
+    public static final String TEST = "src/main/resources/test/data2.txt";
+    public static final String INDEX = "src/main/resources/index";
+    public static final String DOCS = "src/main/resources/test";
 }
--- src/main/java/com/smlteam/textsimilarity/index/TextIndexer.java
+++ src/main/java/com/smlteam/textsimilarity/index/TextIndexer.java
@@ -3,40 +3,126 @@ package com.smlteam.textsimilarity.index;
 import ai.vitk.tok.Tokenizer;
 import ai.vitk.type.Token;
 import com.smlteam.textsimilarity.constant.Constants;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 
 public class TextIndexer {
     public static void main(String[] args){
-        tokenizer();
+        final long startTime = System.nanoTime();
+        String indexPath = Constants.INDEX;
+        String docsPath = Constants.DOCS;
+        boolean create = true;
+
+        if(docsPath == null){
+            System.exit(1);
+        }
+
+        final Path docDir = Paths.get(docsPath);
+//        if (!Files.isReadable(docDir)) {
+//            System.out.println("Document directory '" +docDir.toAbsolutePath()+ "' does not exist or is not readable, please check the path");
+//            System.exit(1);
+//        }
+
+        Date start = new Date();
+        try{
+            System.out.println("Indexing to directory '" + indexPath + "'...");
+            Directory dir = FSDirectory.open(Paths.get(indexPath));
+            Analyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+
+            if (create) {
+                // Create a new index in the directory, removing any
+                // previously indexed documents:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+            } else {
+                // Add new documents to an existing index:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+            }
+            //set RAM size
+            iwc.setRAMBufferSizeMB(256.0);
+
+            IndexWriter writer = new IndexWriter(dir, iwc);
+
+            List<String> documents = new LinkedList<>();
+            documents.add(tokenizer(Constants.ORIGIN));
+            documents.add(tokenizer(Constants.TEST));
+            indexDocs(writer, docDir, documents);
+
+            writer.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        final long duration = System.nanoTime() - startTime;
+        convertTime(TimeUnit.NANOSECONDS.toSeconds(duration));
     }
-    public static void tokenizer(){
+
+    //Tách đoạn văn thành từ hoặc cụm từ
+    public static String tokenizer(String originPath){
+        List<String> doc = new LinkedList<>();
         try {
             Tokenizer tokenizer = new Tokenizer();
-            List<String> tokens = new ArrayList<>();
-            List<List<Token>> tokenList = Files.lines(Paths.get(Constants.TEST))
+            List<List<Token>> tokenList = Files.lines(Paths.get(originPath))
                     .map(s-> tokenizer.tokenize(s))
                     .collect(Collectors.toList());
-            List<List<String>> documents  =new LinkedList<>();
+
             for(List<Token> token : tokenList){
-                List<String> doc = token.stream().map(t->t.getWord()).collect(Collectors.toList());
-                documents.add(doc);
-            }
-            for(List<String> listStr: documents){
-                for(String str: listStr){
-                    System.out.print(str+"\n");
+                for(Token term: token){
+                    doc.add(term.getWord().replace(" ","_"));
                 }
-                System.out.print(listStr);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return doc.toString().replace("[","").replace("]","");
+    }
 
+    //index một tập các văn bản
+    public static void indexDocs(final IndexWriter writer, Path path, List<String> contents) throws IOException {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        for(String content: contents){
+            indexDoc(writer, path, timestamp.getTime(), content);
+        }
+    }
+
+    //index một văn bản
+    public static void indexDoc(IndexWriter writer, Path file, long lastModified, String contents) throws IOException {
+        Document doc = new Document();
+        Field pathField = new StringField("path", file.toString(), Field.Store.YES);
+        doc.add(pathField);
+        doc.add(new LongPoint("modified", lastModified));
+        doc.add(new TextField("contents", contents, Field.Store.YES));
+        if(writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE){
+            System.out.print("Adding file "+ file.toString());
+            writer.addDocument(doc);
+        }else{
+            System.out.print("Updating file "+ file.toString());
+            writer.updateDocument(new Term("path", file.toString()), doc);
+        }
+    }
+    public static void convertTime(long seconds){
+        long min = seconds/60;
+        long sec = seconds-min*60;
+        System.out.print("\n"+ min +" minutes "+sec+" seconds");
     }
 }
